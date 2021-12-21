8d8544d5478b6bfadd482735afafff1aaca64a3a
ImK4Lok <samkalok107@gmail.com>
2021-12-21 22:28:13 +0800

    init xc project

---
+++ HIITFit/HIITFit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HIITFit
+//
+//  Created by KaLok Sam on 21/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HIITFit/HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by KaLok Sam on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
